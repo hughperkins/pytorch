5463402953f775fd0f940aa8b838ae8c830159f3
Hugh Perkins <hughperkins@gmail.com>
2016-02-28 03:29:47 +0800

    add download mnist to run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -9,14 +9,16 @@
 
 source ~/torch/install/bin/torch-activate
 
-if [[ ! -f data/mnist/t10k-labels-idx1-ubyte.gz ]]; then {
+if [[ ! -f data/mnist/train-labels-idx1-ubyte ]]; then {
   echo downloading mnist data...
   mkdir -p data/mnist
   (cd data/mnist
     wget http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz -O train-images-idx3-ubyte.gz
+    gunzip train-images-idx3-ubyte.gz
     wget http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz -O train-labels-idx1-ubyte.gz
-    wget http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz -O t10k-images-idx3-ubyte.gz
-    wget http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz -O t10k-labels-idx1-ubyte.gz
+    gunzip train-labels-idx1-ubyte.gz
+#    wget http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz -O t10k-images-idx3-ubyte.gz
+#    wget http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz -O t10k-labels-idx1-ubyte.gz
   )  
    echo ...downloaded mnist data
 } fi
