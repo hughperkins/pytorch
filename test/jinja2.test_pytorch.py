51bad2cee151d5f3c3dcf4cd780d8a80362ed9f9
Hugh Perkins <hughperkins@gmail.com>
2016-09-08 13:53:56 +0100

    bunch of pep8

--- test/jinja2.test_pytorch.py
+++ test/jinja2.test_pytorch.py
@@ -3,11 +3,10 @@
 
 from __future__ import print_function, division
 import PyTorch
-import array
 import numpy
 import inspect
 
-{% set types = [
+{%- set types = [
     {'Real': 'Long','real': 'long'},
     {'Real': 'Float', 'real': 'float'}, 
     {'Real': 'Double', 'real': 'double'},
@@ -15,27 +14,30 @@ import inspect
 ]
 %}
 
+
 def myeval(expr):
     parent_vars = inspect.stack()[1][0].f_locals
     print(expr, ':', eval(expr, parent_vars))
+
 
 def myexec(expr):
     parent_vars = inspect.stack()[1][0].f_locals
     print(expr)
     exec(expr, parent_vars)
 
-{% for typedict in types %}
-{% set Real = typedict['Real'] %}
-{% set real = typedict['real'] %}
+
+{% for typedict in types -%}
+{%- set Real = typedict['Real'] -%}
+{%- set real = typedict['real'] -%}
 def test_pytorch{{Real}}():
     PyTorch.manualSeed(123)
     numpy.random.seed(123)
 
     {{Real}}Tensor = PyTorch.{{Real}}Tensor
 
-    {% if Real == 'Float' %}
-    A = numpy.random.rand(6).reshape(3,2).astype(numpy.float32)
-    B = numpy.random.rand(8).reshape(2,4).astype(numpy.float32)
+    {% if Real == 'Float' -%}
+    A = numpy.random.rand(6).reshape(3, 2).astype(numpy.float32)
+    B = numpy.random.rand(8).reshape(2, 4).astype(numpy.float32)
 
     C = A.dot(B)
     print('C', C)
@@ -68,9 +70,9 @@ def test_pytorch{{Real}}():
     print('A.dot(B)', A.dot(B))
 
     print('tensorA[2]', tensorA[2])
-    {% endif %}
+    {% endif -%}
 
-    D = PyTorch.{{Real}}Tensor(5,3).fill(1)
+    D = PyTorch.{{Real}}Tensor(5, 3).fill(1)
     print('D', D)
 
     D[2][2] = 4
@@ -79,25 +81,25 @@ def test_pytorch{{Real}}():
     D[3].fill(9)
     print('D', D)
 
-    D.narrow(1,2,1).fill(0)
+    D.narrow(1, 2, 1).fill(0)
     print('D', D)
 
-    {% if Real in ['Float', 'Double'] %}
-    print(PyTorch.{{Real}}Tensor(3,4).uniform())
-    print(PyTorch.{{Real}}Tensor(3,4).normal())
-    print(PyTorch.{{Real}}Tensor(3,4).cauchy())
-    print(PyTorch.{{Real}}Tensor(3,4).exponential())
-    print(PyTorch.{{Real}}Tensor(3,4).logNormal())
-    {% endif %}
-    print(PyTorch.{{Real}}Tensor(3,4).bernoulli())
-    print(PyTorch.{{Real}}Tensor(3,4).geometric())
-    print(PyTorch.{{Real}}Tensor(3,4).geometric())
+    {% if Real in ['Float', 'Double'] -%}
+    print(PyTorch.{{Real}}Tensor(3, 4).uniform())
+    print(PyTorch.{{Real}}Tensor(3, 4).normal())
+    print(PyTorch.{{Real}}Tensor(3, 4).cauchy())
+    print(PyTorch.{{Real}}Tensor(3, 4).exponential())
+    print(PyTorch.{{Real}}Tensor(3, 4).logNormal())
+    {% endif -%}
+    print(PyTorch.{{Real}}Tensor(3, 4).bernoulli())
+    print(PyTorch.{{Real}}Tensor(3, 4).geometric())
+    print(PyTorch.{{Real}}Tensor(3, 4).geometric())
     PyTorch.manualSeed(3)
-    print(PyTorch.{{Real}}Tensor(3,4).geometric())
+    print(PyTorch.{{Real}}Tensor(3, 4).geometric())
     PyTorch.manualSeed(3)
-    print(PyTorch.{{Real}}Tensor(3,4).geometric())
+    print(PyTorch.{{Real}}Tensor(3, 4).geometric())
 
-    print(type(PyTorch.{{Real}}Tensor(2,3)))
+    print(type(PyTorch.{{Real}}Tensor(2, 3)))
 
     size = PyTorch.LongStorage(2)
     size[0] = 4
@@ -122,233 +124,221 @@ def test_pytorch{{Real}}():
     myeval('{{Real}}Tensor().nElement()')
     myeval('{{Real}}Tensor(1).nElement()')
 
-    A = {{Real}}Tensor(3,4).geometric(0.9)
+    A = {{Real}}Tensor(3, 4).geometric(0.9)
     myeval('A')
     myexec('A += 3')
     myeval('A')
     myexec('A *= 3')
     myeval('A')
-    {% if Real != 'Byte' %}
+    {% if Real != 'Byte' -%}
     myexec('A -= 3')
-    {% endif %}
+    {% endif -%}
     myeval('A')
-    {% if Real in ['Float', 'Double'] %}
+    {% if Real in ['Float', 'Double'] -%}
     myexec('A /= 3')
-    {% else %}
+    {% else -%}
     myexec('A //= 3')
-    {% endif %}
+    {% endif -%}
     myeval('A')
 
     myeval('A + 5')
-    {% if Real != 'Byte' %}
+    {% if Real != 'Byte' -%}
     myeval('A - 5')
-    {% endif %}
+    {% endif -%}
     myeval('A * 5')
-    {% if Real in ['Float', 'Double'] %}
+    {% if Real in ['Float', 'Double'] -%}
     myeval('A / 2')
-    {% else %}
+    {% else -%}
     myeval('A // 2')
-    {% endif %}
+    {% endif -%}
 
     B = {{Real}}Tensor().resizeAs(A).geometric(0.9)
     myeval('B')
     myeval('A + B')
-    {% if Real != 'Byte' %}
+    {% if Real != 'Byte' -%}
     myeval('A - B')
-    {% endif %}
+    {% endif -%}
     myexec('A += B')
     myeval('A')
-    {% if Real != 'Byte' %}
+    {% if Real != 'Byte' -%}
     myexec('A -= B')
     myeval('A')
-    {% endif %}
+    {%- endif %}
+
 
 def test_pytorch_{{Real}}_constructors():
     {{Real}}Tensor = PyTorch.{{Real}}Tensor
-    a = {{Real}}Tensor(3,2,5)
+    a = {{Real}}Tensor(3, 2, 5)
     assert(len(a.size()) == 3)
-    a = {{Real}}Tensor(3,2,5,6)
+    a = {{Real}}Tensor(3, 2, 5, 6)
     assert(len(a.size()) == 4)
+
 
 def test_Pytorch_{{Real}}_operator_plus():
     {{Real}}Tensor = PyTorch.{{Real}}Tensor
-    a = {{Real}}Tensor(3,2,5)
-    b = {{Real}}Tensor(3,2,5)
-    {%if Real in ['Float', 'Double'] %}
+    a = {{Real}}Tensor(3, 2, 5)
+    b = {{Real}}Tensor(3, 2, 5)
+    {%if Real in ['Float', 'Double'] -%}
     a.uniform()
     b.uniform()
-    {% else %}
+    {% else -%}
     a.geometric(0.9)
     b.geometric(0.9)
-    {% endif %}
+    {% endif -%}
     res = a + b
-    for i in range(3*2*5):
+    for i in range(3 * 2 * 5):
         assert(abs(res.storage()[i] - (a.storage()[i] + b.storage()[i])) < 0.000001)
+
 
 def test_Pytorch_{{Real}}_operator_plusequals():
     {{Real}}Tensor = PyTorch.{{Real}}Tensor
-    a = {{Real}}Tensor(3,2,5)
-    b = {{Real}}Tensor(3,2,5)
-    {%if Real in ['Float', 'Double'] %}
+    a = {{Real}}Tensor(3, 2, 5)
+    b = {{Real}}Tensor(3, 2, 5)
+    {%if Real in ['Float', 'Double'] -%}
     a.uniform()
     b.uniform()
-    {% else %}
+    {% else -%}
     a.geometric(0.9)
     b.geometric(0.9)
-    {% endif %}
+    {% endif -%}
     res = a.clone()
     res += b
-    for i in range(3*2*5):
+    for i in range(3 * 2 * 5):
         assert(abs(res.storage()[i] - (a.storage()[i] + b.storage()[i])) < 0.000001)
 
-{% if Real not in ['Byte'] %}
+
+{% if Real not in ['Byte'] -%}
 def test_Pytorch_{{Real}}_operator_minus():
     {{Real}}Tensor = PyTorch.{{Real}}Tensor
-    a = {{Real}}Tensor(3,2,5)
-    b = {{Real}}Tensor(3,2,5)
-    {%if Real in ['Float', 'Double'] %}
+    a = {{Real}}Tensor(3, 2, 5)
+    b = {{Real}}Tensor(3, 2, 5)
+    {%if Real in ['Float', 'Double'] -%}
     a.uniform()
     b.uniform()
-    {% else %}
+    {% else -%}
     a.geometric(0.9)
     b.geometric(0.9)
-    {% endif %}
+    {% endif -%}
     res = a - b
-    for i in range(3*2*5):
+    for i in range(3 * 2 * 5):
         assert(abs(res.storage()[i] - (a.storage()[i] - b.storage()[i])) < 0.000001)
-{% endif %}
+{%- endif %}
 
-{% if Real not in ['Byte'] %}
+
+{% if Real not in ['Byte'] -%}
 def test_Pytorch_{{Real}}_operator_minusequals():
     {{Real}}Tensor = PyTorch.{{Real}}Tensor
-    a = {{Real}}Tensor(3,2,5)
-    b = {{Real}}Tensor(3,2,5)
-    {%if Real in ['Float', 'Double'] %}
+    a = {{Real}}Tensor(3, 2, 5)
+    b = {{Real}}Tensor(3, 2, 5)
+    {%if Real in ['Float', 'Double'] -%}
     a.uniform()
     b.uniform()
-    {% else %}
+    {% else -%}
     a.geometric(0.9)
     b.geometric(0.9)
-    {% endif %}
+    {% endif -%}
     res = a.clone()
     res -= b
-    for i in range(3*2*5):
+    for i in range(3 * 2 * 5):
         assert(abs(res.storage()[i] - (a.storage()[i] - b.storage()[i])) < 0.000001)
-{% endif %}
+{%- endif %}
 
-#def test_Pytorch_{{Real}}_cmul():
-#    {{Real}}Tensor = PyTorch.{{Real}}Tensor
-#    a = {{Real}}Tensor(3,2,5)
-#    b = {{Real}}Tensor(3,2,5)
-#    {%if Real in ['Float', 'Double'] %}
-#    a.uniform()
-#    b.uniform()
-#    {% else %}
-#    a.geometric(0.9)
-#    b.geometric(0.9)
-#    {% endif %}
-#    res = a.cmul(b)
-#    for i in range(3*2*5):
-#        {% if Real == 'Byte' %}
-#        assert(abs(res.storage()[i] - ((a.storage()[i] * b.storage()[i])) % 256) < 0.000001)
-#        {% else %}
-#        assert(abs(res.storage()[i] - (a.storage()[i] * b.storage()[i])) < 0.000001)
-#        {% endif %}
 
 def test_Pytorch_{{Real}}_cmul():
     {{Real}}Tensor = PyTorch.{{Real}}Tensor
-    a = {{Real}}Tensor(3,2,5)
-    b = {{Real}}Tensor(3,2,5)
-    {%if Real in ['Float', 'Double'] %}
+    a = {{Real}}Tensor(3, 2, 5)
+    b = {{Real}}Tensor(3, 2, 5)
+    {%if Real in ['Float', 'Double'] -%}
     a.uniform()
     b.uniform()
-    {% else %}
+    {% else -%}
     a.geometric(0.9)
     b.geometric(0.9)
-    {% endif %}
-    res = a.clone() #.cmul(b)
+    {% endif -%}
+    res = a.clone()  # .cmul(b)
     res.cmul(b)
-    for i in range(3*2*5):
-        {% if Real == 'Byte' %}
+    for i in range(3 * 2 * 5):
+        {% if Real == 'Byte' -%}
         assert(abs(res.storage()[i] - ((a.storage()[i] * b.storage()[i])) % 256) < 0.000001)
-        {% else %}
+        {% else -%}
         assert(abs(res.storage()[i] - (a.storage()[i] * b.storage()[i])) < 0.000001)
-        {% endif %}
+        {%- endif %}
 
-#{% for func in ['abs', 'tanh', 'sigmoid', 'neg', 'cinv'] %}
-#{% if Real in ['Double', 'Float'] %}
-#def test_Pytorch_{{Real}}_{{func}}():
-#    {{Real}}Tensor = PyTorch.{{Real}}Tensor
-#    a = {{Real}}Tensor(3,2,5)
-#    {%if Real in ['Float', 'Double'] %}
-#    a.uniform()
-#    {% else %}
-#    a.geometric(0.9)
-#    {% endif %}
-#    res = a.clone()
-#    res.{{func}}()
-#    for i in range(3*2*5):
-#        {% if Real == 'Byte' %}
-#        assert(abs(res.storage()[i] - ((torch.{{func}}(a.storage()[i]) ) % 256) < 0.000001)
-#        {% else %}
-#        assert(abs(res.storage()[i] - (torch.{{func}}(a.storage()[i]))) < 0.000001)
-#        {% endif %}
-#{% endif %}
-#{% endfor %}
+
+{% for func in ['abs', 'tanh', 'sigmoid', 'neg', 'cinv'] -%}
+{% if Real in ['Double', 'Float'] -%}
+# def test_Pytorch_{{Real}}_{{func}}():
+#     {{Real}}Tensor = PyTorch.{{Real}}Tensor
+#     a = {{Real}}Tensor(3,2,5)
+#     {%if Real in ['Float', 'Double'] -%}
+#     a.uniform()
+#     {% else -%}
+#     a.geometric(0.9)
+#     {% endif -%}
+#     res = a.clone()
+#     res.{{func}}()
+#     for i in range(3*2*5):
+#         {% if Real == 'Byte' -%}
+#         assert(abs(res.storage()[i] - ((torch.{{func}}(a.storage()[i]) ) % 256) < 0.000001)
+#         {% else -%}
+#         assert(abs(res.storage()[i] - (torch.{{func}}(a.storage()[i]))) < 0.000001)
+#         {% endif -%}
+{%- endif %}
+{%- endfor %}
+
 
 def test_Pytorch_{{Real}}_operator_div():
     {{Real}}Tensor = PyTorch.{{Real}}Tensor
-    a = {{Real}}Tensor(3,2,5)
-    b = {{Real}}Tensor(3,2,5)
-    {%if Real in ['Float', 'Double'] %}
+    a = {{Real}}Tensor(3, 2, 5)
+    b = {{Real}}Tensor(3, 2, 5)
+    {%if Real in ['Float', 'Double'] -%}
     a.uniform()
     b.uniform()
-    {% else %}
+    {% else -%}
     a.geometric(0.9)
     b.geometric(0.9)
-    {% endif %}
-    {% if Real in ['Float', 'Double'] %}
+    {% endif -%}
+    {% if Real in ['Float', 'Double'] -%}
     res = a / b
-    for i in range(3*2*5):
+    for i in range(3 * 2 * 5):
         assert(abs(res.storage()[i] - (a.storage()[i] / b.storage()[i])) < 0.00001)
-    {% else %}
+    {% else -%}
     # res = a / b  # whilst this should proably be allowed/implemented, it's not yet...
     # for i in range(3*2*5):
     #     assert(abs(res.storage()[i] - (a.storage()[i] / b.storage()[i])) < 0.00001)
     res = a // b
-    for i in range(3*2*5):
+    for i in range(3 * 2 * 5):
         assert(abs(res.storage()[i] - (a.storage()[i] // b.storage()[i])) < 0.00001)
-    {% endif %}
+    {%- endif %}
+
 
 def test_Pytorch_{{Real}}_operator_divequals():
     {{Real}}Tensor = PyTorch.{{Real}}Tensor
-    a = {{Real}}Tensor(3,2,5)
-    b = {{Real}}Tensor(3,2,5)
-    {%if Real in ['Float', 'Double'] %}
+    a = {{Real}}Tensor(3, 2, 5)
+    b = {{Real}}Tensor(3, 2, 5)
+    {%if Real in ['Float', 'Double'] -%}
     a.uniform()
     b.uniform()
-    {% else %}
+    {% else -%}
     a.geometric(0.9)
     b.geometric(0.9)
-    {% endif %}
+    {% endif -%}
     res = a.clone()
-    {% if Real in ['Float', 'Double'] %}
+    {% if Real in ['Float', 'Double'] -%}
     res /= b
-    for i in range(3*2*5):
+    for i in range(3 * 2 * 5):
         assert(abs(res.storage()[i] - (a.storage()[i] / b.storage()[i])) < 0.00001)
-    {% else %}
+    {% else -%}
     res //= b
-    for i in range(3*2*5):
+    for i in range(3 * 2 * 5):
         assert(abs(res.storage()[i] - (a.storage()[i] // b.storage()[i])) < 0.00001)
-    {% endif %}
-
+    {%- endif %}
 
 {% endfor %}
 
 if __name__ == '__main__':
-    {% for typedict in types %}
-    {% set Real = typedict['Real'] %}
-    {% set real = typedict['real'] %}
+    {% for typedict in types -%}
+    {% set Real = typedict['Real'] -%}
+    {% set real = typedict['real'] -%}
     test_pytorch{{Real}}()
     {% endfor %}
-
