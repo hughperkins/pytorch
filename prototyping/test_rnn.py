73a08dc3bd432b4be6460e7bf8e21ceb97ea90ff
Hugh Perkins <hughperkins@gmail.com>
2016-09-12 12:41:25 +0100

    pep8

--- prototyping/test_rnn.py
+++ prototyping/test_rnn.py
@@ -1,10 +1,9 @@
 from __future__ import print_function
 import PyTorch
-
+from PyTorchAug import nn
 PyTorch.require('rnn')
 
-from PyTorchAug import nn
 
-lstm = nn.LSTM(3,4)
-print('lstm', lstm)
-
+if __name__ == '__main__':
+    lstm = nn.LSTM(3, 4)
+    print('lstm', lstm)
