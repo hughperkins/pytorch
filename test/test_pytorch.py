c07b14bad4ef6f43efac6bc008991bc6ca1d9990
Hugh Perkins <hughperkins@gmail.com>
2016-09-12 12:46:39 +0100

    minor factorization of myexec, myeval

--- test/test_pytorch.py
+++ test/test_pytorch.py
@@ -5,17 +5,7 @@ from __future__ import print_function, division
 import PyTorch
 import numpy
 import inspect
-
-
-def myeval(expr):
-    parent_vars = inspect.stack()[1][0].f_locals
-    print(expr, ':', eval(expr, parent_vars))
-
-
-def myexec(expr):
-    parent_vars = inspect.stack()[1][0].f_locals
-    print(expr)
-    exec(expr, parent_vars)
+from test.test_helpers import myeval, myexec
 
 
 def test_pytorchLong():
